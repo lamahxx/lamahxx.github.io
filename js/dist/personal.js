31b9d516a29ac4d4268fc386bfd247862af83fe7
lamahxx <lamahxx@users.noreply.github.com>
2019-01-20 16:52:07 +0100

    try

--- js/dist/personal.js
+++ js/dist/personal.js
@@ -6,3 +6,19 @@ $(document).ready(function() {
       .removeClass("active");
   });
 });
+
+function UrlExists(fullUrl) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", fullUrl, true);
+  xhr.onload = function(e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status != 404) {
+        $("#theatre").append("<img src='" + fullUrl + "'>");
+      } else {
+        $("#theatre").append("<p>Nothing found</p>");
+      }
+    }
+  };
+
+  xhr.send(null);
+}
